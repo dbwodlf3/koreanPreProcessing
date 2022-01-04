ac521ad0359e260aace078d489db12d554116913
Cogi <dbwodlf3@naver.com>
2022-01-04 15:44:34 +0900

    fix module path

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
-import { extractWordsFromUrl } from "lib/data";
-import { insertSpaceBetweenWords } from "lib/middleware";
-import { detectParticle, getCoda, getNucleus, getOnset } from "lib/word";
+import { extractWordsFromUrl } from "./lib/data";
+import { insertSpaceBetweenWords } from "./lib/middleware";
+import { detectParticle, getCoda, getNucleus, getOnset } from "./lib/word";
 
 export default {
     words: {
